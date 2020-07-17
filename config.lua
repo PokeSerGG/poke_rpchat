e691f6e293e692d5f093c146db7c9b421aa7e1a7
Poke <47903164+PokeSer@users.noreply.github.com>
2020-07-17 03:42:48 +0200

    Add files via upload

--- config.lua
+++ config.lua
@@ -3,4 +3,4 @@ Config = {}
 Config.UseDiscord = true
 
 Config.ServerName = 'DesterradosRP'
-Config.WebHook = 'https://discordapp.com/api/webhooks/727935346997985361/FykFEh-5mLsKsTZ_TUnzOiaAHs1_tlW-co0fOVVu4PuxS2HntMOe7Vp4NxTT9FXZgKBS'
+Config.WebHook = ''
